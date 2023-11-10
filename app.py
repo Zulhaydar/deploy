27118f05f831536c0db4e66ad04c0b944cb813c3
zulhaydar fathurrahman sidiq <zulhaydarfathurrahmansidiq@gmail.com>
2023-11-10 17:33:31 +0700

    First Commit

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import Flask, request, render_template, redirect, url_for, jsonify
 from pymongo import MongoClient
 import requests
 from datetime import datetime
 from bson import ObjectId
 
-app = Flask(__name__)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
 
 password = "testku"
 cxn_str = f"mongodb+srv://test:{password}@cluster0.adomazn.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
@@ -12,6 +22,7 @@ client = MongoClient(cxn_str)
 
 db = client.dbsparta_plus_week2
 
+app = Flask(__name__)
 
 @app.route("/")
 def main():
